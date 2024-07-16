a51b47fcf39a7ef3cb931dc1f60f81b4adcffe48
Bohdan <bogdangorlac@gmail.com>
2024-07-16 16:43:12 +0300

    Update firebaseConfig.ts

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -2,17 +2,26 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 //import * as firebaseui from "firebaseui";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBXZGGegIREw2tvsZgpcKR66_oBpvk0DFI",
-  authDomain: "miniapptgpaymentloginvideo.firebaseapp.com",
-  //authDomain: "adapted-worm-one.ngrok-free.app",
-  //authDomain: "t.me/MiniAppTgPaymentLoginVideo_bot/MiniAppTgPaymentLoginVideo",
-  projectId: "miniapptgpaymentloginvideo",
-  storageBucket: "miniapptgpaymentloginvideo.appspot.com",
-  messagingSenderId: "184797749474",
-  appId: "1:184797749474:web:d2c5c08bd6f3c705c57ebb",
-  measurementId: "G-B07RDEGCW6",
+  apiKey: "AIzaSyACiQ0l7nbCrfpEcJYKAmB1J408rfYP5to",
+  authDomain: "telegram-test-mini-apps.firebaseapp.com",
+  projectId: "telegram-test-mini-apps",
+  storageBucket: "telegram-test-mini-apps.appspot.com",
+  messagingSenderId: "326311676737",
+  appId: "1:326311676737:web:2069d6bc88fb5aa882efc0"
 };
+//const firebaseConfig = {
+//  apiKey: "AIzaSyBXZGGegIREw2tvsZgpcKR66_oBpvk0DFI",
+//  authDomain: "miniapptgpaymentloginvideo.firebaseapp.com",
+//  //authDomain: "adapted-worm-one.ngrok-free.app",
+//  //authDomain: "t.me/MiniAppTgPaymentLoginVideo_bot/MiniAppTgPaymentLoginVideo",
+//  projectId: "miniapptgpaymentloginvideo",
+//  storageBucket: "miniapptgpaymentloginvideo.appspot.com",
+//  messagingSenderId: "184797749474",
+//  appId: "1:184797749474:web:d2c5c08bd6f3c705c57ebb",
+//  measurementId: "G-B07RDEGCW6",
+//};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
